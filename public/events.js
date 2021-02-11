da12dba8be74cf48d649dd213d7490c55cec3e8f
Dominic Patrick <dom.patrick92@gmail.com>
2021-02-11 17:47:06 -0500

    partial step 5

--- public/events.js
+++ public/events.js
@@ -2,13 +2,16 @@ window.addEventListener('DOMContentLoaded', () => {
     firstPicture();
 })
 
+let form = document.querySelector(".comment-form")
+
+
 const firstPicture = async () => {
     document.querySelector(".loader").innerHTML = "LOADING..."
     const response = await fetch("/kitten/image")
     const json = await response.json();
-    console.log(json)
+    // console.log(json)
     if (response.ok) {
-        
+
         let catPic = document.querySelector(".cat-pic")
         catPic.src = json.src;
     } else {
@@ -35,16 +38,17 @@ const upVoting = async () => {
      });
     const json = await res.json()
     if (res.ok) {
-       score.innerHTML = json.score 
+       score.innerHTML = json.score
     } else {
         document.querySelector(".error").innerHTML =
           "🚫Something went wrong! Please try again!🚫";
     }
-    
+
 }
+
 const downVoting = async () => {
   const res = await fetch("/kitten/downvote", {
-    method: "PATCH",
+    method: "PATCH"
   });
   const json = await res.json();
   if (res.ok) {
@@ -53,4 +57,30 @@ const downVoting = async () => {
     document.querySelector(".error").innerHTML =
       "🚫Something went wrong! Please try again!🚫";
   }
-};
+}
+//   console.log(form)
+
+  form.addEventListener("submit", (event) => {
+    const formData = new FormData(form)
+    const comment = formData.get("user-comment")
+    console.log(formData)
+      event.preventDefault();
+      callbackComment(comment)
+  })
+
+  const callbackComment = async (comment) => {
+    let commentBox = document.querySelector(".comments")
+
+      const res = await fetch("/kitten/comments", {
+          method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({comment})
+      });
+      const json = await res.json()
+      console.log(json)
+      console.log({comments})
+    //   commentBox.push(json.comments)
+
+  }
+
+
