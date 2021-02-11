26d1feb8ccf4932d1e4e58226755505733ab9369
Tristan San Juan <tristansj88@gmail.com>
2021-02-11 13:42:51 -0700

    finished phase 2

--- public/events.js
+++ public/events.js
@@ -3,10 +3,13 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 const firstPicture = async () => {
+    document.querySelector(".loader").innerHTML = "LOADING..."
     const response = await fetch("/kitten/image")
     const json = await response.json();
     console.log(json)
     let catPic = document.querySelector(".cat-pic")
     catPic.src = json.src;
+    document.querySelector(".loader").innerHTML = "";
 };
 
+document.getElementById('new-pic').addEventListener('click', firstPicture)
