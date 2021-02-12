623c8d45bf1de9f87339f93845548e679ba7b144
Tristan San Juan <tristansj88@gmail.com>
2021-02-11 17:01:35 -0700

    finished project and part of bonus

--- public/events.js
+++ public/events.js
@@ -15,7 +15,8 @@ const firstPicture = async () => {
         let catPic = document.querySelector(".cat-pic")
         catPic.src = json.src;
     } else {
-        document.querySelector('.error').innerHTML = "🚫Something went wrong! Please try again!🚫"
+      document.querySelector('.error').innerHTML = json.message
+      // "🚫Something went wrong! Please try again!🚫"
     }
     document.querySelector(".loader").innerHTML = "";
 };
@@ -40,8 +41,8 @@ const upVoting = async () => {
     if (res.ok) {
        score.innerHTML = json.score
     } else {
-        document.querySelector(".error").innerHTML =
-          "🚫Something went wrong! Please try again!🚫";
+        document.querySelector(".error").innerHTML = json.message
+          // "🚫Something went wrong! Please try again!🚫";
     }
 
 }
@@ -54,8 +55,8 @@ const downVoting = async () => {
   if (res.ok) {
     score.innerHTML = json.score;
   } else {
-    document.querySelector(".error").innerHTML =
-      "🚫Something went wrong! Please try again!🚫";
+    document.querySelector(".error").innerHTML = json.message
+      //"🚫Something went wrong! Please try again!🚫";
   }
 }
 //   console.log(form)
@@ -68,18 +69,39 @@ const downVoting = async () => {
       callbackComment(comment)
   })
 
-  const callbackComment = async (comment) => {
-    let commentBox = document.querySelector(".comments")
+const callbackComment = async (comment) => {
+  let commentBox = document.querySelector(".comments")
 
-      const res = await fetch("/kitten/comments", {
-          method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({comment})
-      });
-      const json = await res.json()
-      console.log(json)
-      console.log({comments})
-    //   commentBox.push(json.comments)
+  const res = await fetch("/kitten/comments", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ comment })
+  });
+  const json = await res.json()
+  console.log(json.comments)
+  console.log({ comment })
+
+  //  commentBox.innerHTML = commentBox.innerHTML + [comment] + "<br>"; 
+  
+  // const commentAdd = document.createElement('div')
+  // commentAdd.classList.add("comment-add")
+  // commentAdd.innerHTML = [comment]
+  // commentBox.appendChild(commentAdd)
+  // const deleteButton = document.createElement('button')
+  // deleteButton.classList.add('delete-button')
+  // deleteButton.setAttribute('id', "delete-button")
+  // deleteButton.innerHTML = 'delete'
+  // commentAdd.appendChild(deleteButton)
+  
+    const commentAdd = document.createElement('div')
+    commentAdd.classList.add("comment-add")
+    commentAdd.innerHTML = json.comments[json.comments.length-1]
+    commentBox.appendChild(commentAdd)
+    const deleteButton = document.createElement('button')
+    deleteButton.classList.add('delete-button')
+    deleteButton.setAttribute('id', "delete-button")
+    deleteButton.innerHTML = 'delete'
+    commentAdd.appendChild(deleteButton)
 
   }
 
